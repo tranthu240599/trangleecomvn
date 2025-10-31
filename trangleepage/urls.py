4911ee609cab369637cdd211637bf06e9b958965
Minh Tri <minhtri.win@gmail.com>
2025-10-31 15:58:22 +0700

    Lan 1

--- trangleepage/urls.py
+++ trangleepage/urls.py
@@ -1,15 +1,17 @@
 from django.urls import path
 from . import views
-from .views import upload_image,delete_image
+from .views import upload_image
 from django.conf import settings
 from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('upload/', upload_image, name='upload_image'),
     path('upload/delete/<int:id>/', views.delete_image, name='delete_image'),
     path('gioi-thieu/', views.gioi_thieu, name='gioi_thieu'),
-
+    path('tuyen-dung/', views.tuyen_dung, name='tuyen_dung'),
+    path('lien-he/', views.lien_he, name='lien_he'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- trangleepage/views.py
+++ trangleepage/views.py
@@ -1,19 +1,41 @@
 from django.shortcuts import render, redirect
 from .models import  GalleryImage
+from django.core.mail import send_mail
 
 def home(request):
     images = GalleryImage.objects.all()
     return render(request, 'home.html', {'images': images})
+
+#-----------GIỚI THIỆU--------------------
 def gioi_thieu(request):
     return render(request, 'gioi-thieu.html')
 def gioi_thieu_view(request):
     return render(request, 'trang_chu.html', {'active_section': 'gioi_thieu'})
+def tuyen_dung(request):
+    return render(request, 'tuyen-dung.html', {'active_section': 'tuyen_dung'})
+def lien_he(request):
+    if request.method == 'POST':
+        ho_ten = request.POST['ho_ten']
+        email = request.POST['email']
+        sdt = request.POST['so_dien_thoai']
+        noi_dung = request.POST['noi_dung']
+        send_mail(
+            subject=f"Yêu cầu từ {ho_ten}",
+            message=f"SĐT: {sdt}\nEmail: {email}\n\nNội dung:\n{noi_dung}",
+            from_email='web@tranglee.vn',
+            recipient_list=['lienhe@tranglee.vn'],
+        )
+        return redirect('cam_on')
+    return render(request, 'lien-he.html', {'active_section': 'lien_he'})
 
-def su_kien_view(request):
-    return render(request, 'trang_chu.html', {'active_section': 'su_kien'})
 
-def lien_he_view(request):
-    return render(request, 'trang_chu.html', {'active_section': 'lien_he'})
+
+
+
+
+
+
+
 
 
 #-----------------------Thêm sdile ảnh--------------------
